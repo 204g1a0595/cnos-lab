131d58bbac41ca31c6d4855456ec82f9d587f19c
204g1a0595 <95006189+204g1a0595@users.noreply.github.com>
2022-09-09 15:36:14 +0530

    first file created

---
+++ oslab/experiment1/Sample.c
@@ -0,0 +1,11 @@
+ /* Name Of the Candidate:S.Shabana
+   Roll No: 204G1A0595
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 9-09-2022
+   Date of Execution: 9-09-2022
+*/
+// Source Code
+    #include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
